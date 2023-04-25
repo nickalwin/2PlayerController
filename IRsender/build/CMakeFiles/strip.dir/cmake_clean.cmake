0f1066ef499ba3d45407d43948a2597fe966c691
Dels1n-Rowe <brammonhemius@gmail.com>
2023-04-25 10:57:43 +0200

    basis infrood signaal versturen

---
+++ IRsender/build/CMakeFiles/strip.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  ".lst"
+  "CMakeFiles/strip"
+  "IRsender.eeprom"
+  "IRsender.hex"
+  "IRsender.lss"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/strip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
